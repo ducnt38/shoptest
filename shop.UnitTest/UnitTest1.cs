12cacb3b2585d56de3367e3d36e7ff63938a2ab3
ducnt38 <ducnt.cntt@gmail.com>
2019-09-11 16:39:41 +0700

    update1

---
+++ shop.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace shop.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
