037dbf439ca6598fa4b04a91b231bab830727ac5
ducnt38 <ducnt.cntt@gmail.com>
2020-03-09 17:53:31 +0700

    update 09032020

--- shop.Data/Infrastructure/RepositoryBase.cs
+++ shop.Data/Infrastructure/RepositoryBase.cs
@@ -50,8 +50,82 @@ namespace shop.Data.Infrastructure
 
         public virtual void DeleteMulti(Expression<Func<T, bool>> where)
         {
-            IEnumerable
+            IEnumerable<T> objects = dbSet.Where<T>(where).AsEnumerable();
+            foreach (T obj in objects)
+                dbSet.Remove(obj);
         }
-        #endregion
+
+        public virtual T GetSingleById(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual IEnumerable<T> GetMany(Expression<Func<T,bool>> where, string includes)
+        {
+            return dbSet.Where(where).ToList();
+        }
+
+        public virtual int Count(Expression<Func<T, bool>> where)
+        {
+            return dbSet.Count(where);
+        }
+
+        public IQueryable<T> GetAll(string[] includes = null)
+        {
+            //HENDLE INCLUDES FOR ASSOCIATED OBJECTS IF APPLICABLE
+            if(includes != null && includes.Count() > 0)
+            {
+                var query = dataContext.Set<T>().Include(includes.First());
+                foreach (var include in includes.Skip(1))
+                    query = query.Include(include);
+                return query.AsQueryable();
+            }
+            return dataContext.Set<T>().AsQueryable();
+        }
+
+        public T GetSingleByCondition(Expression<Func<T,bool>> expression, string[] includes = null)
+        {
+            return GetAll(includes).FirstOrDefault(expression);
+        }
+        public virtual IQueryable<T> GetMulti(Expression<Func<T, bool>> predicate, string[] includes = null)
+        {
+            //HANDLE INCLUDES FOR ASSOCIATED OBJECTS IF APPLICABLE
+            if (includes != null && includes.Count() > 0)
+            {
+                var query = dataContext.Set<T>().Include(includes.First());
+                foreach (var include in includes.Skip(1))
+                    query = query.Include(include);
+                return query.Where<T>(predicate).AsQueryable<T>();
+            }
+            return dataContext.Set<T>().Where<T>(predicate).AsQueryable<T>();
+        }
+
+        public virtual IQueryable<T> GetMultiPaging(Expression<Func<T, bool>> predicate, out int total, int index = 0, int size = 0, string[] includes = null)
+        {
+            int skipCount = index * size;
+            IQueryable<T> _resetSet;
+
+            //HANDLE INCLUDES FOR ASSOCIATED OBJECTS IF APPLICABLE
+            if(includes !=null && includes.Count() > 0)
+            {
+                var query = dataContext.Set<T>().Include(includes.First());
+                foreach (var include in includes.Skip(1))
+                    query = query.Include(include);
+                _resetSet = predicate != null ? query.Where<T>(predicate).AsQueryable() : query.AsQueryable();
+            }
+            else
+            {
+                _resetSet = predicate != null ? dataContext.Set<T>().Where<T>(predicate).AsQueryable() : dataContext.Set<T>().AsQueryable();
+            }
+            _resetSet = skipCount == 0 ? _resetSet.Take(size) : _resetSet.Skip(skipCount).Take(size);
+            total = _resetSet.Count();
+            return _resetSet.AsQueryable();
+        }
+
+        public bool CheckContains(Expression<Func<T, bool>> predicate)
+        {
+            return dataContext.Set<T>().Count<T>(predicate) > 0;
+        }
     }
+    #endregion
 }
